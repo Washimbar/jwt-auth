83c80477daf35c0f157dbc4b2ef10da0e78fcf18
WashimBar <washimbari0001@gmail.com>
2023-12-30 22:16:06 +0530

    login sign update

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,63 @@
 const express = require("express");
-const router = express.Router();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userSchema = require("../schemas/userSchema");
+const dotenv = require("dotenv");
 
-router.use("/", (req, res) => {
+dotenv.config();
+const router = express.Router();
+const User = new mongoose.model("User", userSchema);
+
+router.post("/signup", async (req, res) => {
 	try {
-		res.status(200).json({ name: "Washim Bari" });
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const newUser = User({
+			name: req.body.name,
+			userName: req.body.userName,
+			password: hashedPassword,
+		});
+		newUser.save();
+		console.log(req.body);
+		res.status(200).json({
+			message: "User signup successfully",
+		});
 	} catch (error) {
-		res.status(500).json({ error: error.message });
+		res.status(500).json({ error: "Signup failed!" });
+	}
+});
+
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.find({
+			userName: req.body.userName,
+		});
+		if (user.length > 0) {
+			isValidPassword = await bcrypt.compare(
+				req.body.password,
+				user[0].password
+			);
+			if (isValidPassword) {
+				const token = jwt.sign(
+					{
+						userName: user[0].userName,
+						userId: user[0]._id,
+					},
+					process.env.JWT_SECRET,
+					{
+						expiresIn: "1h",
+					}
+				);
+				res.status(200).json({
+					token: token,
+					message: "User login successfully",
+				});
+			}
+		} else {
+			res.status(401).json({ error: "Login failed!" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: "Login failed!" });
 	}
 });
 
---
+++ src/schemas/userSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	userName: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
+});
+
+module.exports = userSchema;
